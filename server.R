2f5b95d6d20f076987ef3e2862f0f8e8be9d391a
1uf3n <lufanw@uw.edu>
2023-08-13 00:30:46 -0700

    Rebecca bar chart

--- server.R
+++ server.R
@@ -5,6 +5,7 @@ library(plotly)
 
 data <- read.csv("MoviesOnStreamingPlatforms.csv")
 
+
 server <- function(input, output) {
   output$interactive_chart <- renderPlotly({
     filtered_data <- data %>%
@@ -20,11 +21,19 @@ server <- function(input, output) {
       Total_Movies = c(total_netflix, total_hulu, total_prime, total_disney)
     )
     
-    
     plot_ly(aggregate_info, x = ~Streaming_Platforms, y = ~Total_Movies, type = "bar", marker = list(color = "random")) %>%
       layout(title = "Total Number of Movies by Streaming Platforms",
              xaxis = list(title = "Streaming Platforms"),
              yaxis = list(title = "Total Movies"))
+  })
+  output$MovieCountOutput <- renderText({
+    selected_year <- input$SelectedYear
+    filtered_data <- data %>%
+      filter(Year == selected_year)
+    
+    total_movies <- nrow(filtered_data)
+    
+    paste("Total movies in", selected_year, ":", total_movies)
   })
 }
 
